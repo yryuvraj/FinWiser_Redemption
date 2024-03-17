a15828a36ffafafbb9e53a753803576608ea05f2
Yuvraj Rathi <yrathi66@gmail.com>
2024-03-17 19:46:43 +0530

    change to 23

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import csv, json, zipfile 
 import requests, PyPDF2
 
-zip_file_url ='https://disclosures-clerk.house.gov/public_disc/financial-pdfs/2024FD.ZIP'
-pdf_file_url = 'https://disclosures-clerk.house.gov/public_disc/ptr-pdfs/2024/'
+zip_file_url ='https://disclosures-clerk.house.gov/public_disc/financial-pdfs/2023FD.ZIP'
+pdf_file_url = 'https://disclosures-clerk.house.gov/public_disc/ptr-pdfs/2023/'
 r = requests.get(zip_file_url)
-zipfile_name = '2024.zip'
+zipfile_name = '2023.zip'
 
 with open(zipfile_name, 'wb') as f:
     f.write(r.content)
@@ -12,7 +12,7 @@ with open(zipfile_name, 'wb') as f:
 with zipfile.ZipFile(zipfile_name) as z:
     z.extractall('.')
 
-with open('2024FD.txt') as f:
+with open('2023FD.txt') as f:
     for line in csv.reader (f, delimiter='\t'): #delimiter - tab 
         if line[1]=='Pelosi':
             date = line [7]
